bfe63b4f83a021b7c26dcaee57e11e66a8659962
Andrew Reitz <aj.reitz@gmail.com>
2024-12-02 15:00:39 -0600

    Update to API 35
    Updates theming around api 35, updates libraries to latest versions, and
    adds a youtube video of the app for the play store

--- mobile/src/main/java/nes/app/ui/theme/Theme.kt
+++ mobile/src/main/java/nes/app/ui/theme/Theme.kt
@@ -31,26 +31,16 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun NesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
