3e8227559bc0c9eaad8f77c3efffc4853a0c0811
Andrew Reitz <aj.reitz@gmail.com>
2025-08-12 10:42:46 -0500

    Updated to Java 21

--- mobile/mobile.gradle.kts
+++ mobile/mobile.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 android {
@@ -55,8 +55,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildTypes {
